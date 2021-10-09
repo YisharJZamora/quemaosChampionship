4e19a682f5fe1dd2a9b3676f225ea043ec5c48fb
Yishar J. Zamora <rahsiy@gmail.com>
2021-10-09 15:20:20 +0100

    Changes to package.json v9

--- src/server.js
+++ src/server.js
@@ -8,12 +8,12 @@ const port = process.env.PORT || 8080;
 app.use(compression());
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/dist/quemaos-championship'));
 
 // Start the app by listening on the default Heroku port
 app.listen(port);
 
 // For all GET requests, send back index.html so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/quemaos-championship/index.html'));
 });
